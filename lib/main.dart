76b2de3d24195d4c8ab44bafda58e5176c96c0cd
paste94 <94paste94@gmail.com>
2022-11-23 16:16:03 +0100

    Login, register e verifica

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -12,6 +13,11 @@ void main() {
       primarySwatch: Colors.blue,
     ),
     home: const HomePage(),
+    // Definisce tutte le named route presenti nell'app
+    routes: {
+      '/login/': (context) => const LoginView(),
+      '/register/': (context) => const RegisterView()
+    },
   ));
 }
 
@@ -20,27 +26,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: ((context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('Verified email');
-              } else {
-                print('Unverified email');
-              }
-              return const Text('DONE');
+              // final user = FirebaseAuth.instance.currentUser;
+              // if (user?.emailVerified ?? false) {
+              //   print('Verified email');
+              // } else {
+              //   print('Unverified email');
+              //   return const VerifyEmailView();
+              // }
+              return LoginView();
             default:
-              return Text('Loading....');
+              return const CircularProgressIndicator();
           }
-        }),
-      ),
-    );
+        }));
   }
 }
